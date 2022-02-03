a170f0e52c42688dea33957cb6d48da77e774f28
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-02-03 14:57:51 -0800

    public folder

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
+import "reflect-metadata"; //! this must be always first
 import express from "express";
 import { InversifyExpressServer } from "inversify-express-utils";
 import morgan from "morgan";
-import "reflect-metadata"; //! this must be always first
 import { container } from "./providers/inversify/container";
 
 const port = process.env.PORT || 5000;
@@ -12,7 +12,8 @@ server.setConfig((app) => {
   // Middlewares ========================================
   app.use(express.json());
   app.use(morgan("dev"));
+  app.use(express.static("public"));
 });
 
-let app = server.build();
+const app = server.build();
 app.listen(port);
